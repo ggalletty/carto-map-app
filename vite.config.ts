e8951fda8f56d6023af37f4ceb63aa58a1b92507
Giammarco Galletti <4139075+ggalletty@users.noreply.github.com>
2023-03-07 23:49:26 +0100

    ci: gh-pages deploy (#1)
    * ci: gh-pages deploy
    
    * fix(map): remove leftover reference
    
    * ci: add missing env variable
    
    * ci: use environments vars
    
    * ci: use env var for specific step
    
    * ci: deploy on pushes to main

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), basicSsl()],
+export default defineConfig(() => {
+  const base = process.env["VITE_BASE_URL"] ?? "/";
+  return { base, plugins: [react(), basicSsl()] };
 });
